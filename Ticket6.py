130fef04f78f8e4102be7f5725235c0972e6ccfe
Derek <derek.ruggirello@gmail.com>
2022-02-04 23:13:43 -0500

    Update Ticket6.py

--- Ticket6.py
+++ Ticket6.py
@@ -1,11 +1,14 @@
 # Revision 1 Feb 4, 2022
 ## Begin Derek J. Ruggirello (Feb 1, 2022)
 
+
 import random
 
-file_In = open("rockyou.txt", "r")
+Password = ""
 
-password = ""
+dictionary = []
+
+dictionary_List = ["123456", "12345", "123456789", "password","iloveyou","princess","1234567","rockyou","12345678","abc123","nicole","daniel","babygirl","monkey","lovely","jessica","654321","michael","ashley","qwerty","111111","iloveu","000000","michelle","tigger","sunshine","chocolate","password1","soccer","anthony","friends","butterfly","purple","angel","jordan","liverpool","justin","loveme"]
 
 i = 0
 
@@ -13,12 +16,17 @@ while i < 40:
     x = random.randint(1, 12)
     j = 0
     while j <= x:
-        password = password + chr(random.randint(63, 122))
+        Password = Password + chr(random.randint(63, 122))
         j+=1
-    print(password)
-    password = ""
+    if Password in dictionary_List:
+        break
+    else:
+        dictionary.append(Password)
+    Password = ""
     i+=1
-print("hello world")
+
+print(dictionary)
+
 # Revision 1 Feb 4, 2022
 ## End Derek J. Ruggirello here
 # Omega Group/ Ram Valud/ Michael Walker/ project greenwood321 #
