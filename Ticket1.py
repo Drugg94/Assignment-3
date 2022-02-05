53be07153b36b1dbbfcd3bba2b23eb16151bf6db
Derek <derek.ruggirello@gmail.com>
2022-02-04 20:22:52 -0500

    Solved ticket 1

---
+++ Ticket1.py
@@ -0,0 +1,18 @@
+# Revision 1 Feb 1, 2022
+## Begin Derek J. Ruggirello (Feb 1, 2022)
+
+# x variable in order to obtain number for solution
+x = input("Please enter a number for x: ((((x+2)*3)-6)/3) \n")
+
+# Type cast string input into int
+x = int(x)
+
+# variable to store answer of equation
+sum = ((((x+2)*3)-6)/3)
+
+# Prints output
+print("((((",x,"+ 2) * 3) - 6) / 3) =", sum)
+
+# Revision 1 Feb 1, 2022
+## End Derek J. Ruggirello here
+# Omega Group/ Ram Valud/ Michael Walker/ project greenwood321 #
