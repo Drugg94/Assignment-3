999fd47086e9360b56b2a37c815c68b909bfe855
Derek <derek.ruggirello@gmail.com>
2022-02-04 20:37:16 -0500

    Solution to ticket 3

---
+++ Ticket3.py
@@ -0,0 +1,19 @@
+# Revision 1 Feb 2, 2022
+## Begin Derek J. Ruggirello (Feb 1, 2022)
+
+# Varible to store input.
+x = input("Please enter anything to be printed as an int, string, and float-point: \n")
+
+print(x)
+
+# Print typecast integer
+print(int(x))
+
+# Print typecast float
+print(float(x))
+
+# It seems that only integers can be printed without errors.
+
+# Revision 1 Feb 2, 2022
+## End Derek J. Ruggirello here
+# Omega Group/ Ram Valud/ Michael Walker/ project greenwood321 #
